f4f0ef296a95b4a1b0ff7f102099461187926d0f
Eduarda <eduardatuboy@gmail.com>
2025-01-26 13:04:14 -0300

    Componentes, favicon, paginas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <link rel="icon" type="image/png" href="%PUBLIC_URL%/favicon.png" />
       <h1>Home Page</h1>
     </div>
   );
