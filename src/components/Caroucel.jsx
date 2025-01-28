df769aa40b5fcbab2eef03e3d9d71b6e20b276ab
Eduarda <eduardatuboy@gmail.com>
2025-01-27 23:38:08 -0300

    Carrocel funcionando

--- src/components/Caroucel.jsx
+++ src/components/Caroucel.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import ArrowLeftRoundedIcon from "@mui/icons-material/ArrowLeftRounded";
+import ArrowRightRoundedIcon from "@mui/icons-material/ArrowRightRounded";
+import "./Caroucel.css";
 
-function Caroucel(){
-    return <h1>Home Page</h1>;
+function Caroucel() {
+  const inicialPage = 0; // página inicial
+  const [selectedPage, setSelectedPage] = useState(inicialPage);
+
+  // Função para avançar ou retroceder na página
+  function onClick(direction) {
+    // Direção -1 para retroceder, 1 para avançar
+    setSelectedPage((prevPage) => {
+      let newPage = prevPage + direction;
+      // Se for menor que 0, vai para a última página
+      if (newPage < 0) newPage = 2; // considerando 3 páginas
+      // Se for maior que 2 (última página), vai para a primeira página
+      if (newPage > 2) newPage = 0;
+      return newPage;
+    });
+  }
+
+  return (
+    <div id="caroucel">
+      <button id="left" onClick={() => onClick(-1)}>
+        <ArrowLeftRoundedIcon />
+      </button>
+
+      <div className="caroucel-pages">
+        <div
+          className="caroucel-page"
+          style={{
+            backgroundColor: "red",
+            transform: `translateX(-${selectedPage * 100}%)`,
+          }}
+        >
+          a
+        </div>
+        <div
+          className="caroucel-page"
+          style={{
+            backgroundColor: "green",
+            transform: `translateX(-${selectedPage * 100}%)`,
+          }}
+        >
+          b
+        </div>
+        <div
+          className="caroucel-page"
+          style={{
+            backgroundColor: "blue",
+            transform: `translateX(-${selectedPage * 100}%)`,
+          }}
+        >
+          c
+        </div>
+      </div>
+
+      <button id="right" onClick={() => onClick(1)}>
+        <ArrowRightRoundedIcon />
+      </button>
+    </div>
+  );
 }
 
 export default Caroucel;
